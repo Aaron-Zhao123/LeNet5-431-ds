03504417e025911c63579a7671361c9dde2e5725
Aaron Zhao <aaron@dhcp-10-249-122-210.eduroam.wireless.private.cam.ac.uk>
2017-02-17 21:18:59 +0000

    dynamic surgery

---
+++ run_asym_prune.py
@@ -0,0 +1,32 @@
+import os
+import training_v5
+# os.system('python training_v3.py -p0')
+# os.system('python training_v3.py -p1')
+# os.system('python training_v3.py -p2')
+# os.system('python training_v3.py -p3')
+# os.system('python training_v3.py -p4')
+# os.system('python training_v3.py -p4')
+# os.system('python training_v3.py -p5')
+
+acc_list = []
+count = 0
+pcov = 90
+pfc = 90
+retrain = 0
+model_tag = 'pcov'+str(pcov)+'pfc'+str(pfc)
+while (count < 10):
+    # print(model_tag)
+    # pfc = pfc+1
+    pcov = pcov+1
+    param = [
+    ('-pcov',pcov),
+    ('-pfc',pfc),
+    ('-m',model_tag)
+    ]
+    acc = training_v5.main(param)
+    model_tag = 'pcov'+str(pcov)+'pfc'+str(pfc)
+    acc_list.append(acc)
+    count = count + 1
+    print (acc)
+
+print('accuracy summary: {}'.format(acc_list))
---
+++ run_ds.py
@@ -0,0 +1,48 @@
+import os
+import training_v6
+# os.system('python training_v3.py -p0')
+# os.system('python training_v3.py -p1')
+# os.system('python training_v3.py -p2')
+# os.system('python training_v3.py -p3')
+# os.system('python training_v3.py -p4')
+# os.system('python training_v3.py -p4')
+# os.system('python training_v3.py -p5')
+
+acc_list = []
+count = 0
+pcov = 92
+pfc = 92
+pcov2 = 92
+pfc2 = 92
+# model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
+# pfc = pfc+1
+# param = [
+# ('-pcov',pcov),
+# ('-pcov2',pcov2),
+# ('-pfc',pfc),
+# ('-pfc2',pfc2),
+# ('-m',model_tag),
+# ('-ponly', True),
+# ('-test', False)
+# ]
+# acc = training_v6.main(param)
+model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
+while (count < 10):
+    pfc = pfc+1
+    param = [
+    ('-pcov',pcov),
+    ('-pcov2',pcov2),
+    ('-pfc',pfc),
+    ('-pfc2',pfc2),
+    ('-m',model_tag)
+    ]
+    acc = training_v6.main(param)
+    model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
+    acc_list.append(acc)
+    count = count + 1
+    if (acc < 0.985):
+        break
+
+print (acc)
+
+print('accuracy summary: {}'.format(acc_list))
