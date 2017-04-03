46bfa96d5fd8aad99efdc14eef60741377701e92
Aaron Zhao <aaron@user-35-130.vpn.net.private.cam.ac.uk>
2017-04-03 23:48:31 +0800

    fix

--- run_ds.py
+++ run_ds.py
@@ -15,7 +15,7 @@ pfc = 99.5
 pcov2 = 96
 pfc2 = 96
 pcov = 0
-pfc = 95
+pfc = 99.
 pcov2 = 0
 pfc2 = 0
 # model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
@@ -35,7 +35,7 @@ retrain_cnt = 0
 learning_rate = 1e-4
 while (count < 8):
     if (retrain_cnt == 0):
-        pfc = pfc + 1
+        pfc = pfc + 0.1
     # pruning
     param = [
     ('-pcov',pcov),
@@ -47,7 +47,7 @@ while (count < 8):
     ('-prune', True),
     ('-train', False),
     ('-parent_dir', './'),
-    ('-recover_rate', 0.1)
+    ('-recover_rate', 0.02)
     ]
     _ = training_ds.main(param)
 
@@ -63,7 +63,7 @@ while (count < 8):
     ('-prune', False),
     ('-train', True),
     ('-parent_dir', './'),
-    ('-recover_rate', 0.1)
+    ('-recover_rate', 0.02)
     ]
     acc = training_ds.main(param)
     acc_list.append(acc)
