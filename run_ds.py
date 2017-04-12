9c318e1704d03612a8ad5274e4ec57d8eebfb084
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-12 15:41:02 +0100

    retrain

--- run_ds.py
+++ run_ds.py
@@ -31,7 +31,7 @@ pfc = 99.5
 pcov2 = 85
 retrain_cnt = 0
 learning_rate = 1e-4
-recover_rates = [0,0,0.1,0.01]
+recover_rates = [0,0.1,0.01,0]
 while (count < 10):
     if (retrain_cnt == 0):
         # pcov2 = pcov2 + 5
