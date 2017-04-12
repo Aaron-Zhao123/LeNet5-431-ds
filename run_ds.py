2b09eb7d6f6664bb5251f5aea176ab5e3b93f8aa
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-12 22:55:24 +0100

    retrain

--- run_ds.py
+++ run_ds.py
@@ -27,7 +27,7 @@ pfc2 = 0
 # ]
 # acc = training_v6.main(param)
 model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(int(round(pfc*10)))+'pfc'+str(pfc2)
-pfc = 99.7
+pfc = 99.6
 pcov2 = 85
 retrain_cnt = 0
 learning_rate = 1e-5
